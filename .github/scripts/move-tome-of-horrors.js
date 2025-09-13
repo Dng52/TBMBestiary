3bac6a3c7593e73dc4e0f54e02cc915568cf5ce7
Devin <dngrenard@gmail.com>
2025-09-13 12:20:02 -0400

    Tome of Horrors Removal

---
+++ .github/scripts/move-tome-of-horrors.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const dataDir = path.join(__dirname, "../../data");
+const oldDataDir = path.join(__dirname, "../../olddata");
+
+// Make sure /olddata exists
+if (!fs.existsSync(oldDataDir)) {
+  fs.mkdirSync(oldDataDir);
+}
+
+const files = fs.readdirSync(dataDir).filter(f => f.endsWith(".json") && f !== "index.json");
+
+files.forEach(file => {
+  const filePath = path.join(dataDir, file);
+  try {
+    const content = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+    if (content.tags && content.tags.includes("tome-of-horrors")) {
+      const newPath = path.join(oldDataDir, file);
+      fs.renameSync(filePath, newPath);
+      console.log(`Moved ${file} to /olddata`);
+    }
+  } catch (err) {
+    console.error(`Could not parse ${file}:`, err.message);
+  }
+});
